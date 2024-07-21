51b8dceb529b27a22eb90f540c8f70d1ba940058
AFaurot <aaron.faurot@csuglobal.edu>
2024-07-21 07:37:36 -0600

    Initial commit for milestone2

--- Milestone2/portfolioProjectMilestone2.py
+++ Milestone2/portfolioProjectMilestone2.py
@@ -1,6 +1,4 @@
 class ItemToPurchase:
-    formatted_list = []
-    temp_list = []
     #Constructor with initial values and defines types
     def __init__(self, item_name="none", item_price=float(0), item_quantity=int(0)):
         self.item_name = item_name
@@ -11,21 +9,15 @@ class ItemToPurchase:
     #Function to format and print the items cost and return the items total
     def print_item_cost(self):
         item_total = round(self.item_price * self.item_quantity, 2)
-        self.temp_list.append(('{} {} @ ${} = ${}'.format
+        output = (('{} {} @ ${} = ${}'.format
                                     (self.item_name,
                                      self.item_quantity,
                                      self.item_price,
                                      item_total)))
-        #print('{} {} @ ${} = ${}'.format(self.item_name, self.item_quantity, self.item_price, item_total))
-        #for i in ItemToPurchase.formatted_list:
-        #   print(i)
-        self.formatted_list = self.temp_list[:]
 
-        return item_total
-    def print_list(self):
+        #Return list object so items are easy to unpack
+        return [item_total, output]
 
-        for i in self.formatted_list[:]:
-            print(i)
 
 class ShoppingCart:
     cart_items = []
@@ -47,23 +39,29 @@ class ShoppingCart:
         else:
             self.cart_items.remove(item_to_del)
 
+    #modify_item is partially implemented. Will be finished in week 8
     def modify_item(self, item_to_purchase):
         if item_to_purchase not in self.cart_items:
             print("Item not found in cart. Nothing modified")
         else :
             print("Modification MENU \n d = Description \n p=price \n q= quantity")
 
+    # stub method to be completed in week 8
     def get_num_items_in_cart(self):
         pass
 
+    # stub method to be completed in week 8
     def get_cost_of_cart(self):
         pass
+
+    # method to print item descriptions
     def print_descriptions(self):
         print("{}'s Shopping Cart - {}".format(self.customer_name, self.current_date))
-        print("\nItem Descriptions")
+        print("Item Descriptions :")
         for item, desc in self.item_description.items() :
             print("{} : {}".format(item, desc))
 
+    # method to print shopping cart total
     def print_total(self):
         print("{}'s Shopping Cart - {}".format(self.customer_name, self.current_date))
         total = self.cart_item_quantity
@@ -75,6 +73,8 @@ class ShoppingCart:
 
 
 def main ():
+    #Formatted list variable used in output
+    formatted_list=[]
     item1_name = input("Enter the item name: ")
     item1_cost = float(input("Enter the item price: "))
     item1_quantity = int(input("Enter the item quantity: "))
@@ -85,14 +85,20 @@ def main ():
     item2_quantity = int(input("Enter the item quantity: "))
     #pass item2 to ItemToPurchase class
     item2 = ItemToPurchase(item2_name, item2_cost, item2_quantity)
+    #add items to my list for formatted output
+    formatted_list.append(ItemToPurchase.print_item_cost(item1)[1])
+    formatted_list.append(ItemToPurchase.print_item_cost(item2)[1])
+
+    #function to neatly print list
+    def print_formatted_list():
+        for i in formatted_list:
+            print(i)
 
     print("TOTAL COST")
-    ItemToPurchase().print_list()
-    #for i in formatted_list:
-     #   print(i)
+    print_formatted_list()
 
     #Variable to ItemToPurchase.print_item_cost() for both items and add the result
-    init_total = (ItemToPurchase.print_item_cost(item1) + ItemToPurchase.print_item_cost(item2))
+    init_total = (ItemToPurchase.print_item_cost(item1)[0] + ItemToPurchase.print_item_cost(item2)[0])
 
     print("Total: ${}".format(init_total))
     print("\nEnd of output for part 1 of portfolio project.\n")
@@ -101,10 +107,6 @@ def main ():
     sc.add_item(item2_name)
     ShoppingCart.cart_item_quantity += item1_quantity
     ShoppingCart.cart_item_quantity += item2_quantity
-    #print(ShoppingCart.cart_items)
-    #print(ShoppingCart.item_description)
-    sc.print_descriptions()
-    sc.print_total()
 
     def print_menu():
         print("\nMenu \na = Add item to cart \nr = Remove item from cart")
@@ -113,6 +115,9 @@ def main ():
         command = input("Enter command :")
         total = 0
         while command != 'q':
+            # Unclear if this implementation of add_item is due this week,
+            # I am starting on it regardless.
+            # Will be finished in week 8
             if command == 'a':
                 item_name = input("Enter the item name: ")
                 if item_name in sc.cart_items:
@@ -121,18 +126,24 @@ def main ():
                     item_cost = float(input("Enter the item price: "))
                     item_quantity = int(input("Enter the item quantity: "))
                     item = ItemToPurchase(item_name, item_cost, item_quantity)
-                    total += (ItemToPurchase.print_item_cost(item))
+                    total += (ItemToPurchase.print_item_cost(item)[0])
+                    formatted_list.append((ItemToPurchase.print_item_cost(item)[1]))
                     sc.add_item(item_name)
                     ShoppingCart.cart_item_quantity += item_quantity
+            ##To be implemented later in week 8
             elif command == 'r':
                 pass
             ##To be implemented later in week 8
             elif command == 'c':
                 pass
+            ##print descriptions
             elif command == 'i':
                 sc.print_descriptions()
+            ##Print cart output
             elif command == 'o':
                 sc.print_total()
+                print_formatted_list()
+                print("Total: ${}".format(init_total + total))
 
             else:
                 print("Invalid command")
@@ -142,7 +153,6 @@ def main ():
             command = input("Enter command :")
 
     print_menu()
-
 
 
 
